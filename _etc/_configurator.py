8692cb39d04486a8e586d28a6f77dc641a00fe30
Mehmet Topsakal <metokal@gmail.com>
2023-03-16 13:43:27 -0400

    typo fix

--- _etc/_configurator.py
+++ _etc/_configurator.py
@@ -18,7 +18,7 @@ if hostname == 'lnc-170365':
         sp.run('echo "c.NotebookApp.port = 8888" >>  jupyter_lab_config_lnc-170365.py', shell=True)
         sp.run('echo "c.LabServerApp.open_browser = False" >>  jupyter_lab_config_lnc-170365.py', shell=True)
         os.makedirs('/home/mt/.jupyter/_ipynb_checkpoints',exist_ok=True)
-        sp.run('echo "c.FileCheckpoints.checkpoint_dir = /home/mt/.jupyter/_ipynb_checkpoints" >>  jupyter_lab_config_lnc-170365.py', shell=True)
+        sp.run('echo "c.FileCheckpoints.checkpoint_dir = \'/home/mt/.jupyter/_ipynb_checkpoints\'" >>  jupyter_lab_config_lnc-170365.py', shell=True)
         
     sp.run('ln -fs /home/mt/mcode/_etc/jupyter_lab_config_lnc-170365.py /home/mt/.jupyter/jupyter_lab_config.py ', shell=True)
 
@@ -40,7 +40,7 @@ if hostname == 'matsci.dne.bnl.gov':
         sp.run('echo "c.NotebookApp.port = 10000" >>  jupyter_lab_config_matsci.py', shell=True)
         sp.run('echo "c.LabServerApp.open_browser = False" >>  jupyter_lab_config_matsci.py', shell=True)
         os.makedirs('/home/mtopsakal/.jupyter/_ipynb_checkpoints',exist_ok=True)
-        sp.run('echo "c.FileCheckpoints.checkpoint_dir = /home/mtopsakal/.jupyter/_ipynb_checkpoints" >>  jupyter_lab_config_matsci.py', shell=True)
+        sp.run('echo "c.FileCheckpoints.checkpoint_dir = \'/home/mtopsakal/.jupyter/_ipynb_checkpoints\'" >>  jupyter_lab_config_matsci.py', shell=True)
         
     sp.run('ln -fs /home/mtopsakal/mcode/_etc/jupyter_lab_config_matsci.py /home/mtopsakal/.jupyter/jupyter_lab_config.py ', shell=True)
         
--- _etc/jupyter_lab_config_lnc-170365.py
+++ _etc/jupyter_lab_config_lnc-170365.py
@@ -1,4 +1,4 @@
 c.ContentsManager.allow_hidden = True
 c.NotebookApp.port = 8888
 c.LabServerApp.open_browser = False
-c.FileCheckpoints.checkpoint_dir = /home/mt/.jupyter/_ipynb_checkpoints
+c.FileCheckpoints.checkpoint_dir = '/home/mt/.jupyter/_ipynb_checkpoints'
